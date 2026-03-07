f4fb317013806c1e011656fcd487f4f177502be9
GJMCharles <91600327+GJMCharles@users.noreply.github.com>
2026-03-07 14:03:25 +0100

    update

--- sources/token/token_list.c
+++ sources/token/token_list.c
@@ -1,39 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   _env.h                                             :+:      :+:    :+:   */
+/*   token_list.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: grcharle <grcharle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/07 14:02:42 by grcharle          #+#    #+#             */
-/*   Updated: 2026/03/07 14:02:46 by grcharle         ###   ########.fr       */
+/*   Created: 2026/03/07 14:01:40 by grcharle          #+#    #+#             */
+/*   Updated: 2026/03/07 14:02:01 by grcharle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _ENV_H
-# define _ENV_H
+#include "_token.h"
 
-# include "libft.h"
-# include <stdio.h>
-
-typedef struct		s_env
-{
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}   t_env;
-
-/**
- * env_list.c
- */
-void		destroy_env_list(t_env **list);
-t_env		*setup_env_list(char **envp);
-
-/**
- * env_node.c
- */
-void		delete_node(t_env **list, t_env *node);
-void		append_node(t_env **list, t_env *node);
-t_env		*create_node(void);
-
-#endif
